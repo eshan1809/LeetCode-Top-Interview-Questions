d02d77da5fb9cd0287146706fa00b18451309e98
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-01-25 00:57:06 +0530

    Create AddTwoNumbers.java

---
+++ Medium Collection/Linked List/AddTwoNumbers.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode list = new ListNode(0), tmp = list;
+        int carry = 0;
+        while(l1 != null && l2 != null){
+            int sum = l1.val + l2.val + carry, add = sum % 10;
+            carry = sum / 10;
+            tmp.next = new ListNode(add);
+            tmp = tmp.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while(l1 != null){
+            int sum = l1.val + carry, add = sum % 10;
+            carry = sum / 10;
+            tmp.next = new ListNode(add);
+            tmp = tmp.next;
+            l1 = l1.next;
+        }
+        while(l2 != null){
+            int sum = l2.val + carry, add = sum % 10;
+            carry = sum / 10;
+            tmp.next = new ListNode(add);
+            tmp = tmp.next;
+            l2 = l2.next;
+        }
+        if(carry > 0)
+            tmp.next = new ListNode(carry);
+        return list.next;
+    }
+}
