5d8809ca28f5bea399a4dc8832dfe2cb8e0a0edb
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-01-24 10:17:49 +0530

    Create ReverseBits.java

---
+++ Easy Collection/Others/ReverseBits.java
@@ -0,0 +1,12 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int ans = 0, j = 0;
+        for(int i = 31; i >= 0; i--){
+            if((n & (1 << i)) != 0)
+                ans |= (1 << j);
+            j++;
+        }
+        return ans;
+    }
+}
