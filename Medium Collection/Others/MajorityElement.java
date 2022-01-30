cd3eb089c124eafac76c0842fde68588cf810a05
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-01-30 14:21:23 +0530

    Create MajorityElement.java

---
+++ Medium Collection/Others/MajorityElement.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ans = 0, count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(count == 0){
+                ans = nums[i];
+                count = 1;
+            } else if(nums[i] == ans)
+                count++;
+            else
+                count--;
+        }
+        return ans;
+    }
+}
