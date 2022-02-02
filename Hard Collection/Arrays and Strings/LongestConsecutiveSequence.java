2a2df7692e7e6fcfd0b532fb0ddb0d7f8994eeb7
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-02-02 09:13:39 +0530

    Update LongestConsecutiveSequence.java

--- Hard Collection/Arrays and Strings/LongestConsecutiveSequence.java
+++ Hard Collection/Arrays and Strings/LongestConsecutiveSequence.java
@@ -5,20 +5,20 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i : nums)
             pq.add(i);
-        int longest = 1, Long = 1, start = pq.remove();
+        int longest = 1, curr = 1, start = pq.remove();
         while(pq.size() > 0){
             if(pq.peek() == start + 1){
-                Long++;
+                curr++;
                 start++;
                 pq.remove();
             } else if(pq.peek() == start)
                 pq.remove();
             else{
-                longest = Math.max(longest, Long);
-                Long = 1;
+                longest = Math.max(longest, curr);
+                curr = 1;
                 start = pq.remove();
             }
         }
-        return Math.max(longest, Long);
+        return Math.max(longest, curr);
     }
 }
