ecfbe9914151883094980bea8dfefb2c75bbb8a0
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-02-07 08:22:46 +0530

    Create SurroundedRegions.java

---
+++ Hard Collection/Trees and Graphs/SurroundedRegions.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void solve(char[][] board) {
+        int n = board.length, m = board[0].length;
+        for(int i = 0; i < n; i++){
+            if(board[i][0] == 'O')
+                dfs(board, i, 0, n, m);
+            if(board[i][m - 1] == 'O')
+                dfs(board, i, m - 1, n, m);
+        }
+      
+        for(int j = 1; j < m - 1; j++){
+            if(board[0][j] == 'O')
+                dfs(board, 0, j, n, m);
+            if(board[n - 1][j] == 'O')
+                dfs(board, n - 1, j, n, m);
+        }
+      
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if(board[i][j] == 'A')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+    
+    private void dfs(char[][] board, int i, int j, int n, int m){
+        if(i < 0 || i >= n || j < 0 || j >= m || board[i][j] != 'O')
+            return;
+        board[i][j] = 'A';
+        dfs(board, i - 1, j, n, m);
+        dfs(board, i + 1, j, n, m);
+        dfs(board, i, j - 1, n, m);
+        dfs(board, i, j + 1, n, m);
+    }
+}
